3e5452cfec04a0a4ee6c53220aa5e79e68460ca9
Camil Staps <info@camilstaps.nl>
2022-03-24 21:19:11 +0100

    Update copyright and license headers

--- ATSAMF/ATSAMF.ino
+++ ATSAMF/ATSAMF.ino
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2020 Camil Staps <pa5et@camilstaps.nl>
+ * Copyright (C) 2020-2022 Camil Staps <pa5et@camilstaps.nl>
  *
  * This code is based on the original software for the SODA POP by Steven
  * Weber, which was copyright (C) 2017 Steven Weber KD1JV
@@ -7,15 +7,15 @@
  *
  ****************************************************************************
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
  *
  *****************************************************************************
  *
--- ATSAMF/buttons.h
+++ ATSAMF/buttons.h
@@ -1,4 +1,19 @@
-/* Copyright (C) 2020 Camil Staps <pa5et@camilstaps.nl> */
+/**
+ * Copyright (C) 2020-2021 Camil Staps <pa5et@camilstaps.nl>
+ *
+ * This is software for the ATSAMF rig. Fore more information, see the
+ * README.md file.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ */
 
 #ifndef _H_BUTTONS
 #define _H_BUTTONS
